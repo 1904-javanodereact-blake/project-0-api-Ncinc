709d343a96c5f64f5e35c20b0499df59b40f4233
Ncinc <nicholascordon1@gmail.com>
2019-04-19 13:40:45 -0400

    update

--- space-ship-api/src/index.ts
+++ space-ship-api/src/index.ts
@@ -25,7 +25,7 @@ app.use(sessionMiddleware);
  */
 app.use('/users', userRouter);
 app.use('/reim', reimRouter);
-//ssh -i 1904.pem ec2-user@ec2-3-17-68-132.us-east-2.compute.amazonaws.com
+// ssh -i 1904.pem ec2-user@ec2-3-17-68-132.us-east-2.compute.amazonaws.com
 
 // start up the application
 // app.listen(8070, () => {
