ad7d9ea53e277d5cc9b876f33ae689a914b627fa
Ncinc <nicholascordon1@gmail.com>
2019-04-19 10:23:29 -0400

    update

--- space-ship-api/src/index.ts
+++ space-ship-api/src/index.ts
@@ -5,6 +5,7 @@ import { sessionMiddleware } from './middleware/session.middleware';
 import { reimRouter } from './routers/reim-router';
 
 const app = express();
+const port = process.env.REPAY_DB_PORT || 8081;
 
 app.use((req, res, next) => {
   console.log(`request made with url: ${req.url} and method: ${req.method}`);
@@ -26,6 +27,10 @@ app.use('/users', userRouter);
 app.use('/reim', reimRouter);
 
 // start up the application
-app.listen(8070, () => {
-  console.log(`application started`);
+// app.listen(8070, () => {
+//   console.log(`application started`);
+// });
+
+app.listen(port, () => {
+  console.log(`application started on port:` + port);
 });
